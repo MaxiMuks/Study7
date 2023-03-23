cd3557f8c9703b8764e417b3802b072e36abfaa5
MaxiMuks <liveranr1294@gmail.com>
2023-03-23 16:26:41 +0900

    22일 Update

--- bootVer/main.js
+++ bootVer/main.js
@@ -10,8 +10,31 @@
 let taskInput = document.getElementById("task_input");
 let addBtn = document.getElementById("add_btn");
 let taskList = [];
-
+let mode = "all";
+let filterList = [];
+let tabs = document.querySelectorAll(".task_tabs div");
+let horizontalUnderLine = document.getElementById("under_line");
+let horizontalMenus = document.querySelectorAll(".all, .ing, .done");
+// console.log(horizontalMenus)
 addBtn.addEventListener("click", addTask);
+
+horizontalMenus.forEach(menu => menu.addEventListener("click", (e) => horizontalIndicator(e)));
+
+function horizontalIndicator(e) {
+    horizontalUnderLine.style.left = e.currentTarget.offsetLeft + "px";
+    horizontalUnderLine.style.width = e.currentTarget.offsetWidth + "px";
+    horizontalUnderLine.style.top = e.currentTarget.offsetTop + 
+                            e.currentTarget.offsetHeight + "px";
+}
+
+
+for(let i=1; i<tabs.length; i++) {
+    tabs[i].addEventListener("click", function(event) {
+        filter(event)
+        }
+    );
+}
+
 
 function addTask() {
     let task = {
@@ -25,19 +48,26 @@ function addTask() {
     render();
 }
 
+// UI업데이트 하는 곳
 function render() {
+    let list = [];
+    if(mode == "all") {
+        list = taskList;
+    } else if(mode == "ing" || mode == "done") {
+        list = filterList;
+    }
     let resultHTML = "";
 
-    for(let i=0; i<taskList.length; i++) {
-        if(taskList[i].isComplete == true) {
+    for(let i=0; i<list.length; i++) {
+        if(list[i].isComplete == true) {
             resultHTML +=  `
                             <div class="task">
-                                <div class="taskDone">${taskList[i].taskContent}</div>
-                                <div>
-                                    <button onclick="CheckBtn('${taskList[i].id}')">
+                                <div class="taskDone">${list[i].taskContent}</div>
+                                <div class="taskBtn">
+                                    <button onclick="CheckBtn('${list[i].id}')">
                                         <i class="fa-solid fa-rotate fa-xl fa-bounce" style="color: #9dafcd;"></i>
                                     </button>
-                                    <button onclick="DeleteBtn()"> 
+                                    <button onclick="DeleteBtn('${list[i].id}')"> 
                                         <i class="fa-regular fa-trash-can fa-xl fa-bounce" style="color: #9a0e0e;"></i>
                                     </button>
                                 </div>
@@ -46,12 +76,12 @@ function render() {
         } else {
             resultHTML +=  `
                             <div class="task">
-                                <div>${taskList[i].taskContent}</div>
+                                <div>${list[i].taskContent}</div>
                                 <div>
-                                    <button onclick="CheckBtn('${taskList[i].id}')">
+                                    <button onclick="CheckBtn('${list[i].id}')">
                                         <i class="fa-solid fa-circle-check fa-xl fa-bounce" style="color: #11e481;"></i>
                                     </button>
-                                    <button onclick="DeleteBtn()"> 
+                                    <button onclick="DeleteBtn('${list[i].id}')"> 
                                         <i class="fa-regular fa-trash-can fa-xl fa-bounce" style="color: #9a0e0e;"></i>
                                     </button>
                                 </div>
@@ -73,11 +103,43 @@ function CheckBtn(id) {
     console.log(taskList);
 }
 
-function DeleteBtn() {
-    
+function DeleteBtn(id) {
+    // console.log("삭제하기", id);
+    for(let i=0; i<taskList.length; i++) {
+        if(taskList[i].id == id) {
+            taskList.splice(i,1);
+            break;
+        }
+    }
+    render();
+}
+
+function filter(event) {
+    // console.log("탭탭", event.target.id);
+    mode = event.target.id;
+    filterList = [];
+
+    if(mode == "all") {
+        render();
+    } else if(mode == "ing") {
+        for(let i=0; i<taskList.length; i++) {
+            if(taskList[i].isComplete == false) {
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+    } else if(mode == "done") {
+        for(let i=0; i<taskList.length; i++) {
+            if(taskList[i].isComplete == true) {
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+    }
+    console.log(filterList);
 }
 
 function randomIdGenerate() {
     // 구글에 generate random id javascript 검색했음
     return '_' + Math.random().toString(36).substring(2, 9);
-}
+};
